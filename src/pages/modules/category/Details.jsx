963b359fda68c9cfc9f6a1cb7f0f96553366a43e
Sayali Rajwade <sayalirajwade077@gmail.com>
2026-02-27 11:26:55 +0530

    Change UI

--- src/pages/modules/category/Details.jsx
+++ src/pages/modules/category/Details.jsx
@@ -1,4 +1,8 @@
 import { useParams } from "react-router-dom";
+import { MapPin } from "lucide-react";
+import { Phone, Star, ImagePlus, Share2 } from "lucide-react";
+
+
 
 function Details() {
   const { id } = useParams();
@@ -8,48 +12,86 @@ function Details() {
 
       {/* Business Title Section */}
       <h1 className="text-3xl font-bold mb-2 text-left">Nikhil Travel Blog</h1>
-      
 
-       {/* Location */}
-          <div className="flex items-center gap-2 text-gray-700 mb-4">
 
-            <span className="text-orange-500">📍</span>
-            <span>Shiv Vihar, Delhi</span>
-            <a href="#" className="text-blue-600 hover:underline ml-2">
+         {/* Location */}
+        <div 
+        className="flex items-center gap-2 text-gray-600">
+        <MapPin size={18} className="text-black-500" />
+        <span>Shiv Vihar, Delhi</span>
+        <a href="#" className="text-blue-600 hover:underline ml-2">
               View Complete Address
             </a>
-          </div>
-
-          {/* Buttons Row */}
-          <div className="flex flex-wrap gap-3 mb-6">
-
-          <button className="bg-indigo-900 text-white px-2 py-2  hover:bg-indigo-800 transition ">
-            Show Number
-          </button>
-
-          <button className="border px-2 py-2  hover:bg-gray-100">
-            Rate Now
-          </button>
-
-          <button className="border px-2 py-2  hover:bg-gray-100 ">
-            Add Photos
-          </button>
-
-          <button className="border px-2 py-2  hover:bg-gray-100 ">
-            Share
-          </button>
-
         </div>
       
+          <div 
+          className="flex items-center gap-2 text-gray-700 mb-4">
+          </div>
+      
+          
 
-      <div className="grid grid-cols-2 md:grid-cols-3 gap-8">
+          {/* Buttons Row */}
+          <div className="flex gap-4 mt-4">
 
+            {/*Number Button */}
+            <button className="flex items-center gap-2 px-5 py-1 rounded-lg 
+            bg-gradient-to-r from-blue-600/90 to-blue-500/90 
+            backdrop-blur-md 
+            text-white font-medium 
+            shadow-lg shadow-blue-500/30
+            hover:scale-105 hover:shadow-xl 
+            transition-all duration-300">
+              <Phone size={18} />
+              Show Number
+            </button>
+        
+            {/* Rate Now */}
+            <button className="flex items-center gap-2 px-5 py-1 rounded-lg 
+            bg-white/30 backdrop-blur-md 
+            border border-white/40 
+            text-gray-800 font-medium
+            shadow-md hover:shadow-lg 
+            hover:bg-white/40 
+            transition-all duration-300">
+              <Star size={18} className="text-orange-500" />
+              Rate Now
+            </button>
+        
+            {/* Add Photos */}
+            <button className="flex items-center gap-2 px-5 py-1 rounded-lg 
+            bg-white/30 backdrop-blur-md 
+            border border-white/40 
+            text-gray-800 font-medium
+            shadow-md hover:shadow-lg 
+            hover:bg-white/40 
+            transition-all duration-300">
+              <ImagePlus size={18} />
+              Add Photos
+            </button>
+        
+            {/* Share */}
+            <button className="flex items-center gap-2 px-5 py-1 rounded-lg 
+            bg-white/30 backdrop-blur-md 
+            border border-white/40 
+            text-gray-800 font-medium
+            shadow-md hover:shadow-lg 
+            hover:bg-white/40 
+            transition-all duration-300">
+              <Share2 size={18} />
+              Share
+            </button>
+  </div>
+
+
+
+        <div 
+        className="grid grid-cols-2 md:grid-cols-3 gap-8">
         {/* LeftContent */}
         <div className="md:col-span-2">
 
           {/* About  */}
-          <h2 className="text-2xl font-semibold mb-4 text-left" >
-            About Us – Nikhil Travel Blog
+          <h2 className="mt-5 text-2xl font-semibold text-left">
+          About Us – Nikhil Travel Blog
           </h2>
 
         <p className="text-gray-700 mb-4 leading-relaxed text-left">
@@ -87,7 +129,7 @@ function Details() {
         {/* Right Sidebar  */}
         <div>
 
-          <div className="bg-[#eef3f3] p-6 rounded-lg shadow-sm">
+          <div className="bg-[#eef3f3] p-7 rounded-lg shadow-sm">
 
             <h3 className="text-lg font-semibold text-gray-800 text-left">
               Get more information from
@@ -113,11 +155,16 @@ function Details() {
 
             <textarea
               placeholder="What is your Requirement?"
-              className="w-full mt-3 p-2 border rounded-lg"
+              className="w-full mt-3 p-2 border rounded-xl"
               rows="4"
             ></textarea>
 
-            <button className="w-full mt-4 bg-indigo-900 text-white py-2 rounded-lg hover:bg-indigo-800 transition">
+            <button className="w-full mt-4 py-3 rounded-xl 
+            bg-gradient-to-r from-indigo-700 to-indigo-500 
+            text-white font-semibold 
+            shadow-lg hover:shadow-indigo-400/40 
+            hover:scale-[1.02] 
+            transition-all duration-300">
               Send Enquiry
             </button>
 
